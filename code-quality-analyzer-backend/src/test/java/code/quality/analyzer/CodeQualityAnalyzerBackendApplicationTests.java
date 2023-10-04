caf4c5d554ab6536498f56402aac2ee32a9e3cd5
Roshni Joshi <rs888392@dal.ca>
2023-10-04 00:00:02 -0300

    19 Added spring boot application project structure

---
+++ code-quality-analyzer-backend/src/test/java/code/quality/analyzer/CodeQualityAnalyzerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package code.quality.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQualityAnalyzerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
