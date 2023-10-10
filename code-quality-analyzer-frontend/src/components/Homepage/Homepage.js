aed30cd727e346c7ecfb9e91d699932741604497
Princess Kachhadiya <pr228942@dal.ca>
2023-10-10 17:34:24 -0300

    feat: Landing Page

---
+++ code-quality-analyzer-frontend/src/components/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Main from "../Main/Main";
+import "./homepage.css";
+
+
+const Homepage = () => {
+    return(
+        <div className="homepage">
+            <Navbar />
+            <Main />
+        </div>
+    );
+}
+export default Homepage;
---
+++ code-quality-analyzer-frontend/src/components/Main/Main.js
@@ -0,0 +1,35 @@
+import React, { useState } from  "react";
+import "./main.css";
+
+
+
+function Main () {
+   
+  
+    
+    return(
+        <div className="main">
+            <div className="col col1">
+                <h2>Analyze Your Code Now!!!</h2>
+                <input
+                  type="text"
+                    placeholder="Paste Your Repository Link"
+                     
+                />
+                <button   type="button">EXECUTE</button>
+            </div>
+            <div className="col">
+                <div className="card card1"></div>
+                <div className="card card2"></div>
+                <div className="card card3"></div>
+                <div className="card card4"></div>
+                <div className="card card5"></div>
+                <div className="card card6"></div>
+            </div>
+        </div>
+    );
+}
+export default Main;
+
+
+
