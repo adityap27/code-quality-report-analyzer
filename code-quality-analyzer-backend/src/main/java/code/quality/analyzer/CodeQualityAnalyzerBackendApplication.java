eb8318dbe9638fe35fb9c200e740789176386d1d
Roshni Joshi <rs888392@dal.ca>
2023-10-21 17:45:27 -0300

    test: 32 Added test for main method

--- code-quality-analyzer-backend/src/main/java/code/quality/analyzer/CodeQualityAnalyzerBackendApplication.java
+++ code-quality-analyzer-backend/src/main/java/code/quality/analyzer/CodeQualityAnalyzerBackendApplication.java
@@ -2,7 +2,9 @@ package code.quality.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"code.quality.analyzer"})
 @SpringBootApplication
 public class CodeQualityAnalyzerBackendApplication {
 
