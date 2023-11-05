039c35dc7dc9e7d62e51a2200b3ad93d4df8d2ca
Roshni Joshi <rs888392@dal.ca>
2023-11-05 18:38:10 -0400

    feat: Added new request object for python trend analysis service call

---
+++ code-quality-analyzer-backend/src/main/java/code/quality/analyzer/model/TrendAnalysisRequest.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package code.quality.analyzer.model;
+
+import java.util.List;
+
+/**
+ * Request object to call python trend analysis service
+ */
+public class TrendAnalysisRequest {
+	private String reportPath;
+	private List<String> commitIds;
+	private String previousCommitId;
+	public String getReportPath() {
+		return reportPath;
+	}
+	public void setReportPath(String reportPath) {
+		this.reportPath = reportPath;
+	}
+	public List<String> getCommitIds() {
+		return commitIds;
+	}
+	public void setCommitIds(List<String> commitIds) {
+		this.commitIds = commitIds;
+	}
+	public String getPreviousCommitId() {
+		return previousCommitId;
+	}
+	public void setPreviousCommitId(String previousCommitId) {
+		this.previousCommitId = previousCommitId;
+	}
+}
