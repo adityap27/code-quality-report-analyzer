aad79d903b15a88a599cf09cd7d547a2371f6c1b
Ubaidullah Muhammad Naeem <ubaid.m.naeem@dal.ca>
2023-11-07 17:07:38 -0400

    test: fix previous tests and add new tests for tdd

--- code_quality_analyzer_analysis/code_quality_analyzer_analysis/trend_analysis/tests/test_analysis.py
+++ code_quality_analyzer_analysis/code_quality_analyzer_analysis/trend_analysis/tests/test_analysis.py
@@ -1,11 +1,12 @@
 import unittest
 from unittest.mock import patch
 
-from code_quality_analyzer_analysis.trend_analysis.analysis import (analyze_smell_files_in_folder_without_top_entities, analyze_commit_folders_in_folder)
+from code_quality_analyzer_analysis.trend_analysis.analysis import (
+    analyze_smell_files_in_folder_without_top_entities, analyze_commit_folders_in_folder, get_smell_commit_changes
+)
 
 
 class TestTrendAnalysis(unittest.TestCase):
-
     # Mocked return values for functions
     analyze_smell_files_mock = {
         "Architecture Smell": None,
@@ -18,10 +19,10 @@ class TestTrendAnalysis(unittest.TestCase):
         "Testability Smell": {
             "smell_distribution": {"Smell3": 5, "Smell4": 8},
             "top_entities": {"EntityC": 7, "EntityD": 10},
-            "total_smells": 17,
+            "total_smells": 13,
         },
         "Test Smell": None,
-        "total": 20
+        "total_smells": 16
     }
 
     analyze_smell_files_empty_mock = {
@@ -30,7 +31,7 @@ class TestTrendAnalysis(unittest.TestCase):
         "Implementation Smell": None,
         "Testability Smell": None,
         "Test Smell": None,
-        "total": 20
+        "total_smells": 0
     }
 
     analyze_smell_files_in_folder_without_top_entities_mock = {
@@ -42,19 +43,19 @@ class TestTrendAnalysis(unittest.TestCase):
         "Implementation Smell": None,
         "Testability Smell": {
             "smell_distribution": {"Smell3": 5, "Smell4": 8},
-            "total_smells": 17,
+            "total_smells": 13,
         },
         "Test Smell": None,
-        "total": 20
+        "total_smells": 16
     }
 
-    analyze_smell_files_in_folder_without_top_entities_mock_empty = {
-        "Architecture Smell": None,
-        "Design Smell": None,
-        "Implementation Smell": None,
-        "Testability Smell": None,
-        "Test Smell": None,
-        "total": 0
+    full_repo_mock = {
+        "full-repo": {
+            "c0": analyze_smell_files_empty_mock,
+            "c1": analyze_smell_files_in_folder_without_top_entities_mock,
+            "c2": analyze_smell_files_empty_mock,
+            "c3": analyze_smell_files_in_folder_without_top_entities_mock,
+        }
     }
 
     @patch("code_quality_analyzer_analysis.trend_analysis.analysis.analyze_smell_files_in_folder",
@@ -71,14 +72,135 @@ class TestTrendAnalysis(unittest.TestCase):
         self.assertEqual(result, self.analyze_smell_files_empty_mock)
 
     @patch("code_quality_analyzer_analysis.trend_analysis.analysis.analyze_smell_files_in_folder_without_top_entities",
-           side_effect=[analyze_smell_files_in_folder_without_top_entities_mock, analyze_smell_files_in_folder_without_top_entities_mock_empty, analyze_smell_files_in_folder_without_top_entities_mock])
-    def test_analyze_commit_folders_in_folder(self, _):
-        result = analyze_commit_folders_in_folder("/sample_folder/", ["c1","c2","c3"], None)
+           side_effect=[analyze_smell_files_empty_mock,
+                        analyze_smell_files_in_folder_without_top_entities_mock,
+                        analyze_smell_files_empty_mock,
+                        analyze_smell_files_in_folder_without_top_entities_mock,
+                        ]
+           )
+    @patch("code_quality_analyzer_analysis.trend_analysis.analysis.get_smell_commit_changes",
+           return_value=full_repo_mock)
+    def test_analyze_commit_folders_in_folder(self, _, __):
+        result = analyze_commit_folders_in_folder("/sample_folder/", ["c1", "c2", "c3"], "c0")
         expected = {
             "full-repo": {
-            "c1" : self.analyze_smell_files_in_folder_without_top_entities_mock,
-            "c2" : self.analyze_smell_files_in_folder_without_top_entities_mock_empty,
-            "c3" : self.analyze_smell_files_in_folder_without_top_entities_mock
+                "c1": self.analyze_smell_files_in_folder_without_top_entities_mock,
+                "c2": self.analyze_smell_files_empty_mock,
+                "c3": self.analyze_smell_files_in_folder_without_top_entities_mock
             }
         }
+        self.assertEqual(result, expected)
+
+    def test_get_smell_commit_changes(self):
+        commits = ["c0", "c1", "c2", "c3"]
+        result = get_smell_commit_changes(self.full_repo_mock, commits)
+        expected = {
+            "full-repo": self.full_repo_mock["full-repo"],
+            "commit_changes": {
+                "c1": {
+                    "Architecture Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Design Smell": {
+                        "smell_distribution": {
+                            "Smell1": 2,
+                            "Smell2": 1
+                        },
+                        "total_smells": 3
+                    },
+                    "Implementation Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Testability Smell": {
+                        "smell_distribution": {
+                            "Smell3": 5,
+                            "Smell4": 8
+                        },
+                        "total_smells": 13
+                    },
+                    "Test Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "total_smells": 16
+                },
+                "c2": {
+                    "Architecture Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Design Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Implementation Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Testability Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Test Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "total_smells": 0
+                },
+                "c3": {
+                    "Architecture Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Design Smell": {
+                        "smell_distribution": {
+                            "Smell1": 2,
+                            "Smell2": 1
+                        },
+                        "total_smells": 3
+                    },
+                    "Implementation Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "Testability Smell": {
+                        "smell_distribution": {
+                            "Smell3": 5,
+                            "Smell4": 8
+                        },
+                        "total_smells": 13
+                    },
+                    "Test Smell": {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "total_smells": 16
+                }
+            }
+        }
+
         self.assertEqual(result, expected)
