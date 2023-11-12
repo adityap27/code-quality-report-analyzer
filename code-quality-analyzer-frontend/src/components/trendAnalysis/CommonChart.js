35c771f7df79694b02357e1d202667f6e74616d5
Dharmil Shah <dh939824@dal.ca>
2023-11-11 21:53:21 -0400

    feat: removed console logs

--- code-quality-analyzer-frontend/src/components/trendAnalysis/CommonChart.js
+++ code-quality-analyzer-frontend/src/components/trendAnalysis/CommonChart.js
@@ -8,7 +8,6 @@ function CommonChart() {
 
   // JSON data provided
   const jsonData = dummyData
-  console.log(jsonData)
 
   const handleDataChange = (event) => {
     setSelectedData(event.target.value)
@@ -19,18 +18,14 @@ function CommonChart() {
     const prepareData = (jsonData) => {
       // Get the data based on the selected option
       const selectedChartData = jsonData[selectedData]
-      console.log('Selected Data', selectedChartData)
 
       // Extracting smell categories from the selected data
       const categories = Object.keys(selectedChartData['commit1'])
-      console.log('Categories', categories)
       // Extracting commits
       const commits = Object.keys(selectedChartData)
-      console.log('Commits', commits)
 
       const datasets = categories.map((category) => {
         const dataPoints = commits.map((commit) => {
-          console.log('Hey there is a change')
           // Check if selectedChartData and its properties exist before accessing
           if (
             selectedChartData &&
@@ -39,7 +34,6 @@ function CommonChart() {
           ) {
             return selectedChartData[commit][category].total_smells
           }
-          // If the expected properties are missing, return a default value or handle the case
           return 0
         })
 
@@ -59,7 +53,6 @@ function CommonChart() {
   }, [selectedData, jsonData])
 
   return (
-    // <div>Hey there. I'm working</div>
     <div>
       <select onChange={handleDataChange}>
         <option value="full_repo">Full Repo</option>
