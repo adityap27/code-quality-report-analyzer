7efa23834009e30074020767a9e4ef2a26d0943a
Aditya Maheshbhai Purohit <aditya.purohit@dal.ca>
2023-11-14 16:36:00 -0400

    test: update test_analyze_commit_folders_in_folder case to check for full_repo_smell_density. Also, create common mock variables.

--- code_quality_analyzer_analysis/code_quality_analyzer_analysis/trend_analysis/tests/test_analysis.py
+++ code_quality_analyzer_analysis/code_quality_analyzer_analysis/trend_analysis/tests/test_analysis.py
@@ -69,6 +69,182 @@ class TestTrendAnalysis(unittest.TestCase):
         }
     }
 
+    full_repo_smell_density_mock = {
+        "full_repo_smell_density": {
+                "c0": {
+                    architecture_smell: None,
+                    design_smell: None,
+                    implementation_smell: None,
+                    testability_smell: None,
+                    test_smell: None,
+                    "total_smells": 0.0,
+                    "user": "user1"
+                },
+                "c1": {
+                    architecture_smell: None,
+                    design_smell: {
+                        "smell_distribution": {
+                            "Smell1": 0.2,
+                            "Smell2": 0.1
+                        },
+                        "total_smells": 0.3
+                    },
+                    implementation_smell: None,
+                    testability_smell: {
+                        "smell_distribution": {
+                            "Smell3": 0.5,
+                            "Smell4": 0.8
+                        },
+                        "total_smells": 1.3
+                    },
+                    test_smell: None,
+                    "total_smells": 1.6,
+                    "user": "user1"
+                },
+                "c2": {
+                    architecture_smell: None,
+                    design_smell: None,
+                    implementation_smell: None,
+                    testability_smell: None,
+                    test_smell: None,
+                    "total_smells": 0.0,
+                    "user": "user1"
+                },
+                "c3": {
+                    architecture_smell: None,
+                    design_smell: {
+                        "smell_distribution": {
+                            "Smell1": 0.2,
+                            "Smell2": 0.1
+                        },
+                        "total_smells": 0.3
+                    },
+                    implementation_smell: None,
+                    testability_smell: {
+                        "smell_distribution": {
+                            "Smell3": 0.5,
+                            "Smell4": 0.8
+                        },
+                        "total_smells": 1.3
+                    },
+                    test_smell: None,
+                    "total_smells": 1.6,
+                    "user": "user1"
+                }
+            }
+    }
+
+    commit_changes_mock = {
+        "commit_changes": {
+                "c1": {
+                    architecture_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    design_smell: {
+                        "smell_distribution": {
+                            "Smell1": 2,
+                            "Smell2": 1
+                        },
+                        "total_smells": 3
+                    },
+                    implementation_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    testability_smell: {
+                        "smell_distribution": {
+                            "Smell3": 5,
+                            "Smell4": 8
+                        },
+                        "total_smells": 13
+                    },
+                    test_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "total_smells": 16,
+                    "user": "user1"
+                },
+                "c2": {
+                    architecture_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    design_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    implementation_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    testability_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    test_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "total_smells": 0,
+                    "user": "user1"
+                },
+                "c3": {
+                    architecture_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    design_smell: {
+                        "smell_distribution": {
+                            "Smell1": 2,
+                            "Smell2": 1
+                        },
+                        "total_smells": 3
+                    },
+                    implementation_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    testability_smell: {
+                        "smell_distribution": {
+                            "Smell3": 5,
+                            "Smell4": 8
+                        },
+                        "total_smells": 13
+                    },
+                    test_smell: {
+                        "smell_distribution": {
+
+                        },
+                        "total_smells": 0
+                    },
+                    "total_smells": 16,
+                    "user": "user1"
+                }
+            }
+    }
+
     pandas_dataframe_mock = pd.DataFrame({
         "Project Name": ["maven", "maven-core", "maven-compact"],
         "Package Name": ["org.apache.maven.api", "org.apache.maven.artifact", "org.apache.maven.toolchain.building"],
@@ -101,9 +277,11 @@ class TestTrendAnalysis(unittest.TestCase):
                         analyze_smell_files_in_folder_without_top_entities_mock,
                         ]
            )
+    @patch("code_quality_analyzer_analysis.trend_analysis.analysis.get_smell_density_full_repo",
+           return_value={**full_repo_mock, **copy.deepcopy(full_repo_smell_density_mock)})
     @patch("code_quality_analyzer_analysis.trend_analysis.analysis.get_smell_commit_changes",
-           return_value=full_repo_mock)
-    def test_analyze_commit_folders_in_folder(self, _, __):
+           return_value={**full_repo_mock, **copy.deepcopy(full_repo_smell_density_mock), **commit_changes_mock})
+    def test_analyze_commit_folders_in_folder(self, _, __, ___):
         result = analyze_commit_folders_in_folder(
             "/sample_folder/", ["c1", "c2", "c3"], "c0",
             ["user1", "user1", "user1"], "user2"
@@ -113,6 +291,16 @@ class TestTrendAnalysis(unittest.TestCase):
                 "c1": self.analyze_smell_files_in_folder_without_top_entities_mock,
                 "c2": self.analyze_smell_files_empty_mock,
                 "c3": self.analyze_smell_files_in_folder_without_top_entities_mock
+            },
+            "full_repo_smell_density": {
+                "c1": self.full_repo_smell_density_mock["full_repo_smell_density"]["c1"],
+                "c2": self.full_repo_smell_density_mock["full_repo_smell_density"]["c2"],
+                "c3": self.full_repo_smell_density_mock["full_repo_smell_density"]["c3"]
+            },
+            "commit_changes": {
+                "c1": self.commit_changes_mock["commit_changes"]["c1"],
+                "c2": self.commit_changes_mock["commit_changes"]["c2"],
+                "c3": self.commit_changes_mock["commit_changes"]["c3"]
             }
         }
         self.assertEqual(result, expected)
@@ -131,114 +319,7 @@ class TestTrendAnalysis(unittest.TestCase):
         result = get_smell_commit_changes(full_repo_mock, commits, users)
         expected = {
             "full_repo": full_repo_mock["full_repo"],
-            "commit_changes": {
-                "c1": {
-                    self.architecture_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.design_smell: {
-                        "smell_distribution": {
-                            "Smell1": 2,
-                            "Smell2": 1
-                        },
-                        "total_smells": 3
-                    },
-                    self.implementation_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.testability_smell: {
-                        "smell_distribution": {
-                            "Smell3": 5,
-                            "Smell4": 8
-                        },
-                        "total_smells": 13
-                    },
-                    self.test_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    "total_smells": 16,
-                    "user": "user1"
-                },
-                "c2": {
-                    self.architecture_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.design_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.implementation_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.testability_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.test_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    "total_smells": 0,
-                    "user": "user1"
-                },
-                "c3": {
-                    self.architecture_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.design_smell: {
-                        "smell_distribution": {
-                            "Smell1": 2,
-                            "Smell2": 1
-                        },
-                        "total_smells": 3
-                    },
-                    self.implementation_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    self.testability_smell: {
-                        "smell_distribution": {
-                            "Smell3": 5,
-                            "Smell4": 8
-                        },
-                        "total_smells": 13
-                    },
-                    self.test_smell: {
-                        "smell_distribution": {
-
-                        },
-                        "total_smells": 0
-                    },
-                    "total_smells": 16,
-                    "user": "user1"
-                }
-            }
+            "commit_changes": self.commit_changes_mock["commit_changes"]
         }
 
         self.assertEqual(result, expected)
@@ -277,70 +358,8 @@ class TestTrendAnalysis(unittest.TestCase):
         }
         expected = {
             "full_repo": full_repo_mock["full_repo"],
-            "full_repo_smell_density": {
-                "c0": {
-                    self.architecture_smell: None,
-                    self.design_smell: None,
-                    self.implementation_smell: None,
-                    self.testability_smell: None,
-                    self.test_smell: None,
-                    "total_smells": 0.0,
-                    "user": "user1"
-                },
-                "c1": {
-                    self.architecture_smell: None,
-                    self.design_smell: {
-                        "smell_distribution": {
-                            "Smell1": 0.2,
-                            "Smell2": 0.1
-                        },
-                        "total_smells": 0.3
-                    },
-                    self.implementation_smell: None,
-                    self.testability_smell: {
-                        "smell_distribution": {
-                            "Smell3": 0.5,
-                            "Smell4": 0.8
-                        },
-                        "total_smells": 1.3
-                    },
-                    self.test_smell: None,
-                    "total_smells": 1.6,
-                    "user": "user1"
-                },
-                "c2": {
-                    self.architecture_smell: None,
-                    self.design_smell: None,
-                    self.implementation_smell: None,
-                    self.testability_smell: None,
-                    self.test_smell: None,
-                    "total_smells": 0.0,
-                    "user": "user1"
-                },
-                "c3": {
-                    self.architecture_smell: None,
-                    self.design_smell: {
-                        "smell_distribution": {
-                            "Smell1": 0.2,
-                            "Smell2": 0.1
-                        },
-                        "total_smells": 0.3
-                    },
-                    self.implementation_smell: None,
-                    self.testability_smell: {
-                        "smell_distribution": {
-                            "Smell3": 0.5,
-                            "Smell4": 0.8
-                        },
-                        "total_smells": 1.3
-                    },
-                    self.test_smell: None,
-                    "total_smells": 1.6,
-                    "user": "user1"
-                }
-            }
+            "full_repo_smell_density": self.full_repo_smell_density_mock["full_repo_smell_density"]
         }
 
         result = get_smell_density_full_repo(full_repo_mock, "test_folder")
-        print(result,expected)
         self.assertDictEqual(result, expected)
