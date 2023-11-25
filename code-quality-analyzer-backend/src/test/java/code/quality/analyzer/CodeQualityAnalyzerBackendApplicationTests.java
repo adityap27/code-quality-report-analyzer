fcb3e36c604a1ecf895cd9699e6d99f07b93125f
Aditya Maheshbhai Purohit <aditya.purohit@dal.ca>
2023-11-24 20:49:05 -0400

    refactor: remove un-necessary test case of main method.

--- code-quality-analyzer-backend/src/test/java/code/quality/analyzer/CodeQualityAnalyzerBackendApplicationTests.java
+++ code-quality-analyzer-backend/src/test/java/code/quality/analyzer/CodeQualityAnalyzerBackendApplicationTests.java
@@ -6,9 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CodeQualityAnalyzerBackendApplicationTests {
 
-	@Test
-	void main() {
-		CodeQualityAnalyzerBackendApplication.main(new String[] {});
-	}
-
 }
